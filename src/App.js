daac20aa3d876fc663fa688c682b94e268b8a7e2
AllenDodul <allendodul6@gmail.com>
2022-08-18 23:46:38 +0600

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.css";
-import { db,auth } from './firebase';
+import Signin from "./Signin";
 
 export default function App() {
   return (
     <div>
       <h1> Chat App </h1>
+      <Signin/>
     </div>
   );
 }
