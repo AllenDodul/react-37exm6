d2a7f5936ad007dca777d5c68e9fbba95d15e280
AllenDodul <allendodul6@gmail.com>
2022-08-18 23:33:17 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-37exm6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-37exm6)
---
+++ src/Firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const firerbaseAp = firebase.initialized ({
+  apiKey: "AIzaSyDrDcypuooy409xbYODF-RuC0-lukXsh6U",
+    authDomain: "chatapp-4454f.firebaseapp.com",
+    projectId: "chatapp-4454f",
+    storageBucket: "chatapp-4454f.appspot.com",
+    messagingSenderId: "23985746781",
+    appId: "1:23985746781:web:99689b0c18eee9bab02eb4",
+      measurementId: "G-RWY2E514SE"
+});
+ 
+const db = firerbaseApp.firestor()
